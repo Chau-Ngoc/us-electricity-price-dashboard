9e811cb3d2d7144902130b42db524b978bc7aebf
Ngoc-Chau <playerzawesome@gmail.com>
2022-04-07 22:37:10 +0700

    Implemented the LUX bootstrap theme, made the map larger and centered and finally, added a DataTable.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
@@ -8,6 +8,7 @@ df = pd.read_csv("electricity.csv")
 avg_price_each_state = (
     df.groupby("US_State").agg({"Residential Price": "mean"}).reset_index()
 )
+
 us_map_fig = px.choropleth(
     data_frame=avg_price_each_state,
     locations="US_State",
@@ -20,18 +21,43 @@ us_map_fig = px.choropleth(
 year_min = df["Year"].min()
 year_max = df["Year"].max()
 
-app = Dash()
+app = Dash(external_stylesheets=[dbc.themes.LUX])
 
 app.layout = html.Div(
     [
         html.H1("Electricity Prices by US States"),
-        dcc.Graph(id="map-graph", figure=us_map_fig),
+        html.Div(
+            html.Div(
+                dcc.Graph(
+                    id="map-graph",
+                    figure=us_map_fig,
+                    style={"width": "100%", "height": "100%"},
+                ),
+                style={
+                    "width": "100%",
+                    "height": "100%",
+                },
+            ),
+            style={
+                "width": "70%",
+                "height": "800px",
+                "margin": "auto",
+                "display": "block",
+                # "border": "3px #5c5c5c solid",
+                "overflow": "hidden",
+            },
+        ),
         dcc.RangeSlider(
             id="year-slider",
             min=year_min,
             max=year_max,
             value=[year_min, year_max],
             marks={f"{i}": f"{i}" for i in range(year_min, year_max + 1)},
+        ),
+        dash_table.DataTable(
+            id="info-table",
+            columns=[{"name": col, "id": col} for col in df.columns],
+            data=df.to_dict("records"),
         ),
     ]
 )
